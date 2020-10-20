ff29f3cf63b85b74b4b8876a3e431d553d809733
budihaa <budiharyono4@gmail.com>
2020-10-20 22:01:43 +0700

    Create UserProfile components

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <UserProfile />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import UserProfile from "./components/UserProfile";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+    components: { UserProfile },
+};
 </script>
 
 <style>
+@import "./../node_modules/reset-css/reset.css";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    background-color: #f3f5fa;
+    height: 100%;
 }
 </style>
---
+++ src/components/UserProfile.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="user-profile">
+        <div class="user-profile__user-panel">
+            <h1 class="user-profile__username">@{{ user.username }}</h1>
+            <div class="user-profile__follower-count">
+                <strong>Followers: </strong> {{ followers }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "UserProfile",
+    data() {
+        return {
+            followers: 0,
+            user: {
+                id: 1,
+                username: "budihaa",
+                firstName: "Budi",
+                lastName: "Haryono",
+                email: "budiharyono4@gmail.com",
+                isAdmin: true,
+            },
+        };
+    },
+    computed: {
+        fullName() {
+            return `${this.user.firstName} ${this.user.lastName}`;
+        },
+    },
+    methods: {
+        followUser() {
+            this.followers++;
+        },
+    },
+    watch: {
+        // Onchange
+        followers(newCounter, oldCounter) {
+            if (newCounter > oldCounter) {
+                // alert('Hey you got new followers!');
+            }
+        },
+    },
+    mounted() {
+        // this.followUser();
+    },
+};
+</script>
+
+<style scoped>
+.user-profile {
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    grid-gap: 50px;
+    padding: 50px 5%;
+}
+
+.user-profile__username {
+    font-size: 4.5vh;
+    margin-bottom: 0.7rem;
+    font-weight: bold;
+}
+
+.user-profile__user-panel {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border: 1px solid #dfe3e8;
+    margin-bottom: auto;
+}
+
+.user-profile__admin-badge {
+    background: rebeccapurple;
+    color: white;
+    border-radius: 5px;
+    margin-right: auto;
+    padding: 0 10px;
+    font-weight: bold;
+}
+
+.user-profile__twoots-wrapper {
+    display: grid;
+    grid-gap: 10px;
+    margin-bottom: auto;
+}
+</style>
